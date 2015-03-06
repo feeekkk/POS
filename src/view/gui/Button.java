33cbcb52389a59d8d83e9609f3f0406ab84ee72a
fpm5022 <Feek@client-130-203-188-128.mobility-up.psu.edu>
2015-03-05 19:42:21 -0500

    init

--- src/view/gui/Button.java
+++ src/view/gui/Button.java
@@ -4,9 +4,16 @@ import java.awt.Color;
 import javax.swing.JButton;
 
 public class Button extends JButton {
-    public Button(String name) {
+    public Button(Parent panel, String name, int x, int y, int width, int height) {
         setText(name);
+        setForeground(Color.WHITE);
         setBackground(Color.BLUE);
         setVisible(true);
+        
+        setBounds(x, y, width, height);
+        
+        addActionListener(panel);
+        panel.add(this);
     }
+
 }
--- src/view/gui/Return.java
+++ src/view/gui/Return.java
@@ -1,11 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
 package view.gui;
 
 import java.awt.event.ActionEvent;
 
-public class InventoryDashboard extends Parent {
+public class Return extends Parent {
 
-    public InventoryDashboard(Frame f) {
+    public Return(Frame f) {
         super(f, true);
     }
 
