2f4c0e4ecfa388566d9ce07ac17c7935fbb5a780
fpm5022 <fpm5022@gmail.com>
2015-03-31 19:26:53 -0400

    employee info fixed

--- src/view/gui/EmployeeInfo.java
+++ src/view/gui/EmployeeInfo.java
@@ -1,19 +1,12 @@
 
 package view.gui;
 
-import controller.employee.EmployeeController;
 import java.awt.event.ActionEvent;
 
 public class EmployeeInfo extends Parent {
-    private final EmployeeController controller;
 
     public EmployeeInfo(Frame f) {
         super(f, true);
-        
-        controller = new EmployeeController(1);
-        
-        Thread t = new Thread(controller);
-        t.start();
     }
 
     @Override
