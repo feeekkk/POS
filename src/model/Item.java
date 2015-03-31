4883cb45675f3aec4fa62c62d070112d8582b501
Tudor Petcu <tpetcu140s@gmail.com>
2015-03-31 19:03:09 -0400

    commit again

--- src/model/Item.java
+++ src/model/Item.java
@@ -6,6 +6,52 @@
 
 package model;
 
-public class Item {
+public class Item extends Model{
+    private String item_name;
+    private String item_id;
+    private double item_price;
+
+    
+    /**
+     * @return the item_name
+     */
+    public String getItem_name() {
+        return item_name;
+    }
+
+    /**
+     * @param item_name the item_name to set
+     */
+    public void setItem_name(String item_name) {
+        this.item_name = item_name;
+    }
+
+    /**
+     * @return the item_id
+     */
+    public String getItem_id() {
+        return item_id;
+    }
+
+    /**
+     * @param item_id the item_id to set
+     */
+    public void setItem_id(String item_id) {
+        this.item_id = item_id;
+    }
+
+    /**
+     * @return the item_price
+     */
+    public double getItem_price() {
+        return item_price;
+    }
+
+    /**
+     * @param item_price the item_price to set
+     */
+    public void setItem_price(double item_price) {
+        this.item_price = item_price;
+    }
 
 }
