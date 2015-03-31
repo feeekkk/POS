897a30eca4bc54f804998e36abce1b0b43c2ad6b
fpm5022 <fpm5022@gmail.com>
2015-03-31 19:05:43 -0400

    employee view

---
+++ src/view/EmployeeView.java
@@ -0,0 +1,23 @@
+package view;
+
+public class EmployeeView extends View{
+    private int id;
+    private String first, last;
+    
+    public EmployeeView(int id, String first, String last) {
+        this.id = id;
+        this.first = first;
+        this.last = last;
+    }
+
+    @Override
+    public void update() {
+    }
+
+    @Override
+    public void display() {
+        System.out.println("ID: " + id);
+        System.out.println("First: " + first);
+        System.out.println("Last: " + last);
+    }
+}
