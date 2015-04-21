dbb45a153993969eb8503b57a9d1443d15b4f600
Mike Stuck <mikestuck@msn.com>
2015-04-21 14:48:50 -0400

    Added Database integration

--- src/app.java
+++ src/app.java
@@ -1,8 +1,23 @@
 import view.gui.Frame;
+import java.sql.*;
 
 public class app {
 
     public static void main(String[] args) {
         Frame f = new Frame();
-    }
+        Connection conn = null;
+           try
+           {
+               String userName = "root";
+               String password = "root";
+               String url = "jdbc:mysql://localhost:8889/POS";
+               Class.forName ("com.mysql.jdbc.Driver").newInstance ();
+               conn = DriverManager.getConnection (url, userName, password);
+               System.out.println ("Database connection established");
+           }
+           catch (Exception e)
+           {
+               System.err.println ("Cannot connect to database server");
+           }
+       }
 }
