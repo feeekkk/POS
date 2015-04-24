01acdd3717a6c5377a97549ab1a156b957a47956
Sean McCloskey <smm6019@gmail.com>
2015-04-23 20:33:07 -0400

    transaction

--- src/mutualModels/Transaction.java
+++ src/mutualModels/Transaction.java
@@ -6,6 +6,54 @@
 
 package mutualModels;
 
-public class Transaction extends Model {
+import java.util.concurrent.LinkedBlockingQueue;
 
+public class Transaction extends Model {
+    
+    private LinkedBlockingQueue items;
+    private Employee employee;
+    private double total;
+
+    /**
+     * @return the items
+     */
+    public LinkedBlockingQueue getItems() {
+        return items;
+    }
+
+    /**
+     * @param items the items to set
+     */
+    public void setItems(LinkedBlockingQueue items) {
+        this.items = items;
+    }
+
+    /**
+     * @return the employee
+     */
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    /**
+     * @param employee the employee to set
+     */
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    /**
+     * @return the total
+     */
+    public double getTotal() {
+        return total;
+    }
+
+    /**
+     * @param total the total to set
+     */
+    public void setTotal(double total) {
+        this.total = total;
+    }
+    
 }
