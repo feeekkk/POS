a837a07cc5a801f92f8490398e031507a9ae840c
fpm5022 <fpm5022@gmail.com>
2015-04-23 20:23:12 -0400

    server purchase handling

--- src/server/MessageReceiver.java
+++ src/server/MessageReceiver.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import mutualModels.Purchase;
 import server.model.Request;
 
 public class MessageReceiver implements Runnable {
@@ -27,12 +28,21 @@ public class MessageReceiver implements Runnable {
     public void checkForMessageFromClient() {
         try {
             Object message;
+            Request request = null;
             while((message = is.readObject()) != null) {
                 if(message instanceof Integer) {
-                    Request request = new Request(client, message);
-                    System.out.println("server: request received from client: " + message + " | submitting request to main server.");
+                    request = new Request(client, message);
+                    System.out.println("server: Item lookup request received from client: " + message + " | submitting request to main server.");
                     server.submitRequest(request);
                 }
+                else if(message instanceof Purchase) {
+                    request = new Request(client, message);
+                    System.out.println("server: Purchase request received from client: " + message + " | submitting request to main server.");
+                }
+                else {
+                    System.err.println("server: unknown request type");
+                }
+                server.submitRequest(request);
             }
         } catch ( IOException | ClassNotFoundException ex) {
             Logger.getLogger(MessageReceiver.class.getName()).log(Level.SEVERE, null, ex);
--- src/server/RequestProcessor.java
+++ src/server/RequestProcessor.java
@@ -3,13 +3,13 @@ package server;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import mutualModels.Item;
+import mutualModels.*;
 import server.dao.ItemDAO;
 import server.model.Request;
 
 public class RequestProcessor {
-    private MainServer server;
-    private ExecutorService service;
+    private final MainServer server;
+    private final ExecutorService service;
     
     public RequestProcessor(MainServer server) {
         this.server = server;
@@ -32,6 +32,11 @@ public class RequestProcessor {
                             request.setReturnObject(item);
                             System.out.println("server: found item: " + item.getItem_name() + "and submitting the response to main server");
                             server.submitResponse(request);
+                        }
+                        else if(o instanceof Purchase) {
+                            Purchase purchase = (Purchase) o;
+                            System.out.println("server: working on purchase");
+                            System.err.println("server: TO DO: actually do purchase stuff");
                         }
                         else {
                             System.err.println("server: unknown request type");
