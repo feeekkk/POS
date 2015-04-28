5fadada0ff6da851d749fbe8a461da619b99dd0e
Tudor Petcu <tpetcu140s@gmail.com>
2015-04-28 13:03:12 -0400

    commit again

--- src/client/gui/EmployeeInfo.java
+++ src/client/gui/EmployeeInfo.java
@@ -1,6 +1,7 @@
 
 package client.gui;
 
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import javax.swing.*;
@@ -18,11 +19,13 @@ public class EmployeeInfo extends Parent {
         
         empName = new JLabel("Employee Name: "+ currentEmp.getFirst()+" " +currentEmp.getLast());
         empName.setFont(new Font("Arial", Font.PLAIN, 24));
+        empName.setForeground(Color.WHITE);
         empName.setBounds(100, 100, this.getWidth(), 50);
         add(empName);
         
         empId = new JLabel("Employee ID: "+ currentEmp.getId());
         empId.setFont(new Font("Arial", Font.PLAIN, 24));
+        empId.setForeground(Color.WHITE);
         empId.setBounds(100, 150, this.getWidth(), 50);
         add(empId);
         
