b5c318bebbcc75a7f897191af9cfb185994cbebd
Tudor Petcu <tpetcu140s@gmail.com>
2015-04-28 12:51:45 -0400

    commit again

--- src/client/gui/InventoryDashboard.java
+++ src/client/gui/InventoryDashboard.java
@@ -4,16 +4,16 @@ package client.gui;
 import java.awt.event.ActionEvent;
 
 public class InventoryDashboard extends Parent {
-    private final Button viewAll, OutofStock, newShip, editProduct;
+    private final Button viewAll, OutofStock, newShip;
 
     public InventoryDashboard(Frame f) {
         super(f, true);
-        int width = 300, height = 200, left = 100, right = 800, top = 150, bottom = 400;
+        int width = 300, height = 200, left = 100, right = 800, top = 150, bottom = 400, center = 450;
         
         viewAll = new Button(this, "View All", left, top, width, height);
         OutofStock = new Button(this, "Out of Stock", right, top, width, height);
-        newShip = new Button(this, "new Shipment", left, bottom, width, height);
-        editProduct = new Button(this, "Edit Product", right, bottom, width, height);
+        newShip = new Button(this, "new Shipment", center, top, width, height);
+        
     }
 
     @Override
@@ -29,9 +29,6 @@ public class InventoryDashboard extends Parent {
         }
         else if (obj == newShip){
             swap(new NewShipmentPanel(frame));
-        }
-        else if (obj == editProduct){
-            swap(new EditProductPanel(frame));
         }
         
     }
--- src/client/gui/NewShipmentPanel.java
+++ src/client/gui/NewShipmentPanel.java
@@ -6,17 +6,64 @@
 
 package client.gui;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class NewShipmentPanel extends Parent{
+    
+    private Button add;
+    private JTextField itemID;
+    private JTextField itemName;
+    private JTextField price;
 
     public NewShipmentPanel(Frame f) {
         super(f, true);
+        init();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+    }
+    
+    public void init(){
+        JLabel idLabel = new JLabel("ID");
+        idLabel.setFont(new Font("Arial", Font.PLAIN, 24));
+        idLabel.setForeground(Color.WHITE);
+        idLabel.setBounds(450, 100, 100, 50);
+        
+        JLabel nameLabel = new JLabel("Name");
+        nameLabel.setFont(new Font("Arial", Font.PLAIN, 24));
+        nameLabel.setForeground(Color.WHITE);
+        nameLabel.setBounds(450, 150, 200, 50);
+        
+        JLabel priceLabel = new JLabel("Price");
+        priceLabel.setFont(new Font("Arial", Font.PLAIN, 24));
+        priceLabel.setForeground(Color.WHITE);
+        priceLabel.setBounds(450, 200, 100, 50);
+        
+        itemID = new JTextField();
+        itemID.setBounds(550, 100, 100, 50);
+        
+        itemName = new JTextField();
+        itemName.setBounds(550, 150, 100, 50);
+        
+        price = new JTextField("$");
+        price.setBounds(550, 200, 100, 50);
+        
+        add = new Button(this, "Add", 550, 250, 100, 50);
+        
+        add(idLabel);
+        add(nameLabel);
+        add(priceLabel);
+        add(itemID);
+        add(itemName);
+        add(price);
+        add(add);
     }
 
 }
